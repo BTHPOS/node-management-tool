42f193a9f2371a204ecf918fb66de7dc4801fe6b
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-07-10 20:47:51 -0400

    Fixing popup consoles

--- src/main.go
+++ src/main.go
@@ -566,8 +566,27 @@ func initWindowEvents(window *gotron.BrowserWindow) {
       })
 }
 
+func hideConsole() {
+    console := w32.GetConsoleWindow()
+    if console == 0 {
+        return // no console attached
+    }
+    // If this application is the process that created the console window, then
+    // this program was not compiled with the -H=windowsgui flag and on start-up
+    // it created a console along with the main application window. In this case
+    // hide the console window.
+    // See
+    // http://stackoverflow.com/questions/9009333/how-to-check-if-the-program-is-run-from-a-console
+    _, consoleProcID := w32.GetWindowThreadProcessId(console)
+    if w32.GetCurrentProcessId() == consoleProcID {
+        w32.ShowWindowAsync(console, w32.SW_HIDE)
+    }
+}
+
 func run() {
 
+    hideConsole()
+    
     // Set APP PATH
     if runtime.GOOS != "windows" {
         APP_PATH = os.Args[1]
