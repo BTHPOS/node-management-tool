77f89260a921be3332db426429a4d773bff21bc5
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-07-11 18:35:43 -0400

    Removed first parameter path check and added gotron for linux

--- src/main.go
+++ src/main.go
@@ -490,9 +490,6 @@ func initWindowEvents(window *gotron.BrowserWindow) {
 
 func run() {
 
-    // Set APP PATH
-    APP_PATH = os.Args[1]
-
     // Create a new browser window instance
     window, err := gotron.New("ui")
     if err != nil {
